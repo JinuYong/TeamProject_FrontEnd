1afd2fea662b8a1e40e1fe49ec38760aae893649
JinuYong <amazinggal27@gmail.com>
2022-07-20 14:04:21 +0900

    kakakomap

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,39 +4,39 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _4e765714 = () => interopDefault(import('..\\pages\\CheckPayment.vue' /* webpackChunkName: "pages/CheckPayment" */))
-const _5f04a3f2 = () => interopDefault(import('..\\pages\\http-common.js' /* webpackChunkName: "pages/http-common" */))
-const _0f62bca1 = () => interopDefault(import('..\\pages\\LogIn.vue' /* webpackChunkName: "pages/LogIn" */))
-const _5ebd2506 = () => interopDefault(import('..\\pages\\MapExample.vue' /* webpackChunkName: "pages/MapExample" */))
-const _1106f4ef = () => interopDefault(import('..\\pages\\mypage\\index.vue' /* webpackChunkName: "pages/mypage/index" */))
-const _47dabd08 = () => interopDefault(import('..\\pages\\Qna.vue' /* webpackChunkName: "pages/Qna" */))
-const _39f5ee06 = () => interopDefault(import('..\\pages\\SearchResult.vue' /* webpackChunkName: "pages/SearchResult" */))
-const _b4be9c9c = () => interopDefault(import('..\\pages\\SignUp\\index.vue' /* webpackChunkName: "pages/SignUp/index" */))
-const _33fcee5c = () => interopDefault(import('..\\pages\\board\\List.vue' /* webpackChunkName: "pages/board/List" */))
-const _43643d91 = () => interopDefault(import('..\\pages\\board\\Write.vue' /* webpackChunkName: "pages/board/Write" */))
-const _2b59134c = () => interopDefault(import('..\\pages\\find\\id.vue' /* webpackChunkName: "pages/find/id" */))
-const _6ea662b6 = () => interopDefault(import('..\\pages\\find\\id\\Complete.vue' /* webpackChunkName: "pages/find/id/Complete" */))
-const _eb35ebe8 = () => interopDefault(import('..\\pages\\find\\password.vue' /* webpackChunkName: "pages/find/password" */))
-const _f5795536 = () => interopDefault(import('..\\pages\\find\\password\\Complete.vue' /* webpackChunkName: "pages/find/password/Complete" */))
-const _4f54f65c = () => interopDefault(import('..\\pages\\inform\\Detail.vue' /* webpackChunkName: "pages/inform/Detail" */))
-const _7a40b4df = () => interopDefault(import('..\\pages\\inform\\List.vue' /* webpackChunkName: "pages/inform/List" */))
-const _a6bc1c70 = () => interopDefault(import('..\\pages\\Items\\Details\\index.vue' /* webpackChunkName: "pages/Items/Details/index" */))
-const _23ded696 = () => interopDefault(import('..\\pages\\Items\\List.vue' /* webpackChunkName: "pages/Items/List" */))
-const _68a77e05 = () => interopDefault(import('..\\pages\\service\\ItemDetailDataService.js' /* webpackChunkName: "pages/service/ItemDetailDataService" */))
-const _a51900ce = () => interopDefault(import('..\\pages\\service\\ReviewDataService.js' /* webpackChunkName: "pages/service/ReviewDataService" */))
-const _38c58d29 = () => interopDefault(import('..\\pages\\service\\ShoppingCartDataService.js' /* webpackChunkName: "pages/service/ShoppingCartDataService" */))
-const _6586980c = () => interopDefault(import('..\\pages\\service\\UserDataService.js' /* webpackChunkName: "pages/service/UserDataService" */))
-const _37bdb668 = () => interopDefault(import('..\\pages\\shoppingCart\\ShoppingCart.vue' /* webpackChunkName: "pages/shoppingCart/ShoppingCart" */))
-const _27b8bf29 = () => interopDefault(import('..\\pages\\SignUp\\Complete.vue' /* webpackChunkName: "pages/SignUp/Complete" */))
-const _53f56c30 = () => interopDefault(import('..\\pages\\Items\\Details\\components\\CardSliderCom.vue' /* webpackChunkName: "pages/Items/Details/components/CardSliderCom" */))
-const _74fd6072 = () => interopDefault(import('..\\pages\\Items\\Details\\components\\ImgGalleryCom.vue' /* webpackChunkName: "pages/Items/Details/components/ImgGalleryCom" */))
-const _7e05cdb0 = () => interopDefault(import('..\\pages\\Items\\Details\\components\\KakaoMapApiCom.vue' /* webpackChunkName: "pages/Items/Details/components/KakaoMapApiCom" */))
-const _c0005bd4 = () => interopDefault(import('..\\pages\\Items\\Details\\components\\ReviewsCom.vue' /* webpackChunkName: "pages/Items/Details/components/ReviewsCom" */))
-const _455f5236 = () => interopDefault(import('..\\pages\\board\\detail\\update\\_idx.vue' /* webpackChunkName: "pages/board/detail/update/_idx" */))
-const _1fecbc2d = () => interopDefault(import('..\\pages\\board\\detail\\_idx.vue' /* webpackChunkName: "pages/board/detail/_idx" */))
-const _e7cdd310 = () => interopDefault(import('..\\pages\\Items\\Details\\_idx.vue' /* webpackChunkName: "pages/Items/Details/_idx" */))
-const _10ee18f1 = () => interopDefault(import('..\\pages\\payment\\_userIdx.vue' /* webpackChunkName: "pages/payment/_userIdx" */))
-const _04e40d8a = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _f3bfcc36 = () => interopDefault(import('..\\pages\\CheckPayment.vue' /* webpackChunkName: "pages/CheckPayment" */))
+const _650cf994 = () => interopDefault(import('..\\pages\\http-common.js' /* webpackChunkName: "pages/http-common" */))
+const _6ab42612 = () => interopDefault(import('..\\pages\\LogIn.vue' /* webpackChunkName: "pages/LogIn" */))
+const _5bb8fa35 = () => interopDefault(import('..\\pages\\MapExample.vue' /* webpackChunkName: "pages/MapExample" */))
+const _0f145d60 = () => interopDefault(import('..\\pages\\mypage\\index.vue' /* webpackChunkName: "pages/mypage/index" */))
+const _a23c99a6 = () => interopDefault(import('..\\pages\\Qna.vue' /* webpackChunkName: "pages/Qna" */))
+const _df3f6328 = () => interopDefault(import('..\\pages\\SearchResult.vue' /* webpackChunkName: "pages/SearchResult" */))
+const _b8a3cbba = () => interopDefault(import('..\\pages\\SignUp\\index.vue' /* webpackChunkName: "pages/SignUp/index" */))
+const _530881e6 = () => interopDefault(import('..\\pages\\board\\List.vue' /* webpackChunkName: "pages/board/List" */))
+const _1e80fa00 = () => interopDefault(import('..\\pages\\board\\Write.vue' /* webpackChunkName: "pages/board/Write" */))
+const _1b532f8a = () => interopDefault(import('..\\pages\\find\\id.vue' /* webpackChunkName: "pages/find/id" */))
+const _78d7f658 = () => interopDefault(import('..\\pages\\find\\id\\Complete.vue' /* webpackChunkName: "pages/find/id/Complete" */))
+const _4e04afbb = () => interopDefault(import('..\\pages\\find\\password.vue' /* webpackChunkName: "pages/find/password" */))
+const _84a22758 = () => interopDefault(import('..\\pages\\find\\password\\Complete.vue' /* webpackChunkName: "pages/find/password/Complete" */))
+const _c815aafe = () => interopDefault(import('..\\pages\\inform\\Detail.vue' /* webpackChunkName: "pages/inform/Detail" */))
+const _279bfa4e = () => interopDefault(import('..\\pages\\inform\\List.vue' /* webpackChunkName: "pages/inform/List" */))
+const _e6074d8e = () => interopDefault(import('..\\pages\\Items\\Details\\index.vue' /* webpackChunkName: "pages/Items/Details/index" */))
+const _7344b172 = () => interopDefault(import('..\\pages\\Items\\List.vue' /* webpackChunkName: "pages/Items/List" */))
+const _49dfe476 = () => interopDefault(import('..\\pages\\service\\ItemDetailDataService.js' /* webpackChunkName: "pages/service/ItemDetailDataService" */))
+const _fb0a72ec = () => interopDefault(import('..\\pages\\service\\ReviewDataService.js' /* webpackChunkName: "pages/service/ReviewDataService" */))
+const _a503c94c = () => interopDefault(import('..\\pages\\service\\ShoppingCartDataService.js' /* webpackChunkName: "pages/service/ShoppingCartDataService" */))
+const _ce363186 = () => interopDefault(import('..\\pages\\service\\UserDataService.js' /* webpackChunkName: "pages/service/UserDataService" */))
+const _039f4d97 = () => interopDefault(import('..\\pages\\shoppingCart\\ShoppingCart.vue' /* webpackChunkName: "pages/shoppingCart/ShoppingCart" */))
+const _fbf495d0 = () => interopDefault(import('..\\pages\\SignUp\\Complete.vue' /* webpackChunkName: "pages/SignUp/Complete" */))
+const _c4ca9dbe = () => interopDefault(import('..\\pages\\Items\\Details\\components\\CardSliderCom.vue' /* webpackChunkName: "pages/Items/Details/components/CardSliderCom" */))
+const _82bab53a = () => interopDefault(import('..\\pages\\Items\\Details\\components\\ImgGalleryCom.vue' /* webpackChunkName: "pages/Items/Details/components/ImgGalleryCom" */))
+const _690926df = () => interopDefault(import('..\\pages\\Items\\Details\\components\\KakaoMapApiCom.vue' /* webpackChunkName: "pages/Items/Details/components/KakaoMapApiCom" */))
+const _9575ca76 = () => interopDefault(import('..\\pages\\Items\\Details\\components\\ReviewsCom.vue' /* webpackChunkName: "pages/Items/Details/components/ReviewsCom" */))
+const _149ebe32 = () => interopDefault(import('..\\pages\\board\\detail\\update\\_idx.vue' /* webpackChunkName: "pages/board/detail/update/_idx" */))
+const _fc276844 = () => interopDefault(import('..\\pages\\board\\detail\\_idx.vue' /* webpackChunkName: "pages/board/detail/_idx" */))
+const _6a0b7ce7 = () => interopDefault(import('..\\pages\\Items\\Details\\_idx.vue' /* webpackChunkName: "pages/Items/Details/_idx" */))
+const _003fe0e2 = () => interopDefault(import('..\\pages\\payment\\_userIdx.vue' /* webpackChunkName: "pages/payment/_userIdx" */))
+const _603576fb = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -51,137 +51,137 @@ export const routerOptions = {
 
   routes: [{
     path: "/CheckPayment",
-    component: _4e765714,
+    component: _f3bfcc36,
     name: "CheckPayment"
   }, {
     path: "/http-common",
-    component: _5f04a3f2,
+    component: _650cf994,
     name: "http-common"
   }, {
     path: "/LogIn",
-    component: _0f62bca1,
+    component: _6ab42612,
     name: "LogIn"
   }, {
     path: "/MapExample",
-    component: _5ebd2506,
+    component: _5bb8fa35,
     name: "MapExample"
   }, {
     path: "/mypage",
-    component: _1106f4ef,
+    component: _0f145d60,
     name: "mypage"
   }, {
     path: "/Qna",
-    component: _47dabd08,
+    component: _a23c99a6,
     name: "Qna"
   }, {
     path: "/SearchResult",
-    component: _39f5ee06,
+    component: _df3f6328,
     name: "SearchResult"
   }, {
     path: "/SignUp",
-    component: _b4be9c9c,
+    component: _b8a3cbba,
     name: "SignUp"
   }, {
     path: "/board/List",
-    component: _33fcee5c,
+    component: _530881e6,
     name: "board-List"
   }, {
     path: "/board/Write",
-    component: _43643d91,
+    component: _1e80fa00,
     name: "board-Write"
   }, {
     path: "/find/id",
-    component: _2b59134c,
+    component: _1b532f8a,
     name: "find-id",
     children: [{
       path: "Complete",
-      component: _6ea662b6,
+      component: _78d7f658,
       name: "find-id-Complete"
     }]
   }, {
     path: "/find/password",
-    component: _eb35ebe8,
+    component: _4e04afbb,
     name: "find-password",
     children: [{
       path: "Complete",
-      component: _f5795536,
+      component: _84a22758,
       name: "find-password-Complete"
     }]
   }, {
     path: "/inform/Detail",
-    component: _4f54f65c,
+    component: _c815aafe,
     name: "inform-Detail"
   }, {
     path: "/inform/List",
-    component: _7a40b4df,
+    component: _279bfa4e,
     name: "inform-List"
   }, {
     path: "/Items/Details",
-    component: _a6bc1c70,
+    component: _e6074d8e,
     name: "Items-Details"
   }, {
     path: "/Items/List",
-    component: _23ded696,
+    component: _7344b172,
     name: "Items-List"
   }, {
     path: "/service/ItemDetailDataService",
-    component: _68a77e05,
+    component: _49dfe476,
     name: "service-ItemDetailDataService"
   }, {
     path: "/service/ReviewDataService",
-    component: _a51900ce,
+    component: _fb0a72ec,
     name: "service-ReviewDataService"
   }, {
     path: "/service/ShoppingCartDataService",
-    component: _38c58d29,
+    component: _a503c94c,
     name: "service-ShoppingCartDataService"
   }, {
     path: "/service/UserDataService",
-    component: _6586980c,
+    component: _ce363186,
     name: "service-UserDataService"
   }, {
     path: "/shoppingCart/ShoppingCart",
-    component: _37bdb668,
+    component: _039f4d97,
     name: "shoppingCart-ShoppingCart"
   }, {
     path: "/SignUp/Complete",
-    component: _27b8bf29,
+    component: _fbf495d0,
     name: "SignUp-Complete"
   }, {
     path: "/Items/Details/components/CardSliderCom",
-    component: _53f56c30,
+    component: _c4ca9dbe,
     name: "Items-Details-components-CardSliderCom"
   }, {
     path: "/Items/Details/components/ImgGalleryCom",
-    component: _74fd6072,
+    component: _82bab53a,
     name: "Items-Details-components-ImgGalleryCom"
   }, {
     path: "/Items/Details/components/KakaoMapApiCom",
-    component: _7e05cdb0,
+    component: _690926df,
     name: "Items-Details-components-KakaoMapApiCom"
   }, {
     path: "/Items/Details/components/ReviewsCom",
-    component: _c0005bd4,
+    component: _9575ca76,
     name: "Items-Details-components-ReviewsCom"
   }, {
     path: "/board/detail/update/:idx?",
-    component: _455f5236,
+    component: _149ebe32,
     name: "board-detail-update-idx"
   }, {
     path: "/board/detail/:idx?",
-    component: _1fecbc2d,
+    component: _fc276844,
     name: "board-detail-idx"
   }, {
     path: "/Items/Details/:idx",
-    component: _e7cdd310,
+    component: _6a0b7ce7,
     name: "Items-Details-idx"
   }, {
     path: "/payment/:userIdx?",
-    component: _10ee18f1,
+    component: _003fe0e2,
     name: "payment-userIdx"
   }, {
     path: "/",
-    component: _04e40d8a,
+    component: _603576fb,
     name: "index"
   }],
 
--- pages/MapExample.vue
+++ pages/MapExample.vue
@@ -1,27 +1,54 @@
 <template>
     <div>
-        <div ref="map" class="map-board"></div>
-        <button @click="searchAddress">주소검색</button>
+        <div class="map-container">
+            <ul>
+                <li v-for="(loc, idx) in locations" :key="idx" @click="positionChange(loc.position, idx)" class="map-list" :class="{activeList:activeList==idx}">
+                    <p class="title">{{loc.title}}</p>
+                    <p class="content">{{loc.content}}</p></li>
+            </ul>
+            <div ref="map" class="map-board">
+                <div class="custom-con">
+                    <p @click="viewAll">전체보기</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            addList: [],
-            coords: []
+            locations: [
+                {title: "흑돼지식당", content: "- "+"맛있는 제주 흑돼지를 먹을 수 있는 숯불구이 전문점", position: "부산 부산진구 중앙대로 672"},
+                {title: "멘도롱카페", content: "- "+"제주감귤을 직접 착즙해 만드는 감귤에이드가 맛있는 카페", position: "부산 부산진구 중앙대로680번가길 80-16"},
+                {title: "제주호텔 임페리얼", content: "- "+"제주바다가 한눈에 보이는 오션뷰의 5성급 호텔", position: "부산 부산진구 동천로 47-1"},
+            ],
+            coords: [],
+            map: null,
+            activeList: null,
         }
     },
     methods: {
-        searchAddress() {
-            new daum.Postcode({
-                oncomplete: (data) => {
-                    this.addList.push(data.address);
-                    this.kakaoMapShow();
-                }
-            }).open();
-            console.log(this.addList);
+        // searchAddress() {
+        //     new daum.Postcode({
+        //         oncomplete: (data) => {
+        //             this.locations.push(data.address);
+        //             this.kakaoMapShow();
+        //         }
+        //     }).open();
+        //     console.log(this.addList);
+        // },
+
+        viewAll() {
+
         },
+        positionChange(pos, idx) {
+            let showLoc = pos;
+            this.map.panTo(showLoc);
+            this.activeList = idx;
+            console.log(idx);
+        },
+
         kakaoMapShow() {
             let container = this.$refs.map;
             let options = {
@@ -29,10 +56,9 @@ export default {
                 level: 3,
             };
             let map = new kakao.maps.Map(container, options);
+            this.map = map;
             let zoomControl = new kakao.maps.ZoomControl();
             map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
-
 
             // let positions = [
             //     {
@@ -75,35 +101,50 @@ export default {
             // }
             // map.setBounds(bound);
 
-            let geocoder = new kakao.maps.services.Geocoder();
-            for (let i = 0; i < this.addList.length; i++) {
-                geocoder.addressSearch(this.addList[i], (result, status) => {
-                    if (status === kakao.maps.services.Status.OK) {
-                        this.coords.push(new kakao.maps.LatLng(result[0].y, result[0].x));
-                        let marker = new kakao.maps.Marker({
-                            map: map,
-                            position: this.coords[0]
-                        })
-                    }
-                    console.log(this.coords);
-                });
+            // let geocoder = new kakao.maps.services.Geocoder();
+            // for (let i = 0; i < this.addList.length; i++) {
+            //     geocoder.addressSearch(this.addList[i], (result, status) => {
+            //         if (status === kakao.maps.services.Status.OK) {
+            //             this.coords.push(new kakao.maps.LatLng(result[0].y, result[0].x));
+            //             let marker = new kakao.maps.Marker({
+            //                 map: map,
+            //                 position: this.coords[0]
+            //             })
+            //         }
+            //         console.log(this.coords);
+            //     });
                 
-            }
+            // }
 
-            let positions = [
-                {
-                    content: '<div class="customoverlay">' + '<a>' + '<span class="text">여행지1</span>' + '</a>' + '</div>',
-                    latlng: new kakao.maps.LatLng(33.452278, 126.567803)
-                },
-                {
-                    content: '<div class="customoverlay">' + '<a>' + '<span class="text">여행지2</span>' + '</a>' + '</div>',
-                    latlng: new kakao.maps.LatLng(33.452671, 126.574792)
-                },
-                {
-                    content: '<div class="customoverlay">' + '<a>' + '<span class="text">여행지3</span>' + '</a>' + '</div>',
-                    latlng: new kakao.maps.LatLng(33.451744, 126.572441)
+            let positions = []
+            let geocoder = new kakao.maps.services.Geocoder();
+            for (let i = 0; i < this.locations.length; i++) {
+                geocoder.addressSearch(this.locations[i].position, (result, status) => {
+                    if (status === kakao.maps.services.Status.OK) {
+                        this.locations[i].position = new kakao.maps.LatLng(result[0].y, result[0].x)
+                }});
+
+                let pos = {
+                    content: '<div class="customoverlay">' + '<a>' + '<span class="text">' + this.locations[i].title + '</span>' + '</a>' + '</div>',
+                    latlng: this.locations[i].position
                 }
-            ];
+                positions.push(pos);
+            }
+            console.log(positions);
+            // let positions = [
+            //     {
+            //         content: '<div class="customoverlay">' + '<a>' + '<span class="text">' + this.locations.title + '</span>' + '</a>' + '</div>',
+            //         latlng: new kakao.maps.LatLng(33.452278, 126.567803)
+            //     },
+            //     {
+            //         content: '<div class="customoverlay">' + '<a>' + '<span class="text">' + this.locations.title + '</span>' + '</a>' + '</div>',
+            //         latlng: new kakao.maps.LatLng(33.452671, 126.574792)
+            //     },
+            //     {
+            //         content: '<div class="customoverlay">' + '<a>' + '<span class="text">' + this.locations.title + '</span>' + '</a>' + '</div>',
+            //         latlng: new kakao.maps.LatLng(33.451744, 126.572441)
+            //     }
+            // ];
             let bound = new kakao.maps.LatLngBounds();
             for (let i = 0; i < positions.length; i++) {
                 const marker = new kakao.maps.Marker({
@@ -116,11 +157,8 @@ export default {
                     content: positions[i].content,
                     // yAnchor: 1
                 });
-
                 bound.extend(positions[i].latlng);
             };
-            
-            
             map.setBounds(bound);
         },
         
@@ -132,9 +170,53 @@ export default {
 }
 </script>
 <style>
+    .map-list {
+        width: 500px;
+        height: 100px;
+        list-style: none;
+        padding: 10px 10px;
+        margin: 10px;
+        border: 1px solid lightgray;
+        box-shadow: rgb(0 0 0 / 15%) 0px 2px 2px 0px;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+    .activeList {
+        border: 2px solid #a30000;
+    }
+    .map-list .title{
+        font-weight: 500;
+        font-size: 16px;
+        padding: 10px 0 5px 10px;
+        margin: 0;
+    }
+    .map-list .content{
+        font-weight: 300;
+        font-size: 14px;
+        padding: 5px 0 5px 10px;
+        margin: 0;
+    }
+    .map-container {
+        display: flex;
+    }
     .map-board {
-        width: 800px;
+        width: 500px;
         height: 400px;
+    }
+    .custom-con {
+        position: absolute;
+        background: #fff;
+        right: 40px;
+        top: 5px;
+        z-index: 100;
+        border-radius: 10px;
+        text-align: center;
+        width: 80px;
+        height: 35px;
+        box-shadow: rgb(0 0 0 / 15%) 0px 2px 2px 0px;
+        line-height: 35px;
+        font-size: 14px;
+        font-weight: 500;
     }
     .customoverlay {
         position:relative;
@@ -163,8 +245,8 @@ export default {
         font-size:14px;
         font-weight:bold;
         overflow:hidden;
-        background: #d95050;
-        background: #d95050 
+        background: #a30000;
+        background: #a30000
                     url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png) 
                     no-repeat right 14px center;
         }
